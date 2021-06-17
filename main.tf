0c7ac53b3ade5c6405382def4c61d428017147ab
felipe <felipej9805@gmail.com>
2021-06-17 16:17:12 -0500

    change token

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "azurerm" {
 provider "github" {
   version = "~> 4.0"
   #token = var.token # or `GITHUB_TOKEN`
-  token = "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"
+  token = "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"
 
 }
 
